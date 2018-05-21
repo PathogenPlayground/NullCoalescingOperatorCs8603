b133feab3d56046093547c1fc636b32dff1c15af
David Maas <contact@pathogenstudios.com>
2018-05-21 07:41:02 -0500

    Initial commit demonstrating the issue.

---
+++ NullCoalescingOperatorCs8603/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace NullCoalescingOperatorCs8603
+{
+    class Foo
+    {
+        private static int _nextInstanceNumber = 0;
+        public int InstanceNumber { get; }
+
+        public Foo()
+        {
+            InstanceNumber = _nextInstanceNumber;
+            _nextInstanceNumber++;
+        }
+
+        public override string ToString()
+            => $"Foo #{InstanceNumber}";
+    }
+
+    class Program
+    {
+        private static Foo? _foo = null;
+        public static Foo FooProperty
+        {
+            get => _foo ?? (_foo = new Foo()); // warning CS8603: Possible null reference return.
+        }
+
+        public static Foo FooProperty2
+        {
+            get
+            {
+                Foo? ret = null;
+                return ret ?? (ret = new Foo()); // warning CS8603: Possible null reference return.
+            }
+        }
+
+        public static Foo FooProperty3
+        {
+            get => _foo ?? new Foo(); // No warning
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FooProperty);
+            Console.WriteLine(FooProperty);
+            Console.WriteLine(FooProperty2);
+            Console.WriteLine(FooProperty2);
+            Console.WriteLine(FooProperty3);
+            Console.WriteLine(FooProperty3);
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,25 @@
+This repository demonstrates a bug in the 05/14/18 C# [Nullable Reference Types Preview](https://github.com/dotnet/csharplang/wiki/Nullable-Reference-Types-Preview) with Visual Studio 2017 15.7.1. It corresponds to [dotnet/roslyn#27009](https://github.com/dotnet/roslyn/issues/27009).
+
+# Project Setup
+
+The project features a handful of properties on `Program` that return non-null references using the null coalescing operator:
+
+Property|Description
+----|----
+FooProperty|Lazy-initializes a field inside the right-hand side of a null coalescing operator
+FooProperty2|"Lazy-initializes" a local variable inside the right-hand side of a null coalescing operator
+FooProperty3|Returns a field or a new instance using a null coalescing operator
+
+(`FooProperty2` exists to make sure this problem isn't related to [dotnet/roslyn#9978](https://github.com/dotnet/roslyn/issues/9978).)
+
+# Actual Behavior
+
+The compiler reports the following warning for `FooProperty` and `FooProperty2`:
+
+```
+warning CS8603: Possible null reference return.
+```
+
+# Expected Behavior
+
+No warning is reported because the assignment causes the `_foo` field/`ret` variable to be initialized with a non-null value.
